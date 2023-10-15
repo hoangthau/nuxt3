d611d2e76305afbadcc8fcfbd802272c8076188d
Thau Nguyen <nghoangthau@gmail.com>
2023-10-15 10:55:02 +0700

    feat: #1 update vue plugins

--- plugins/vue-scrollto.ts
+++ plugins/vue-scrollto.ts
@@ -1,17 +1,7 @@
 import VueScrollTo from 'vue-scrollto';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive('scroll-to', {
-    mounted(el, binding) {
-      const { element, duration } = binding.value;
-      el.addEventListener('click', (e: Event) => {
-        e.preventDefault();
-        const elem = document.querySelector(binding.value);
-        VueScrollTo.scrollTo(elem, duration, { easing: [0.12, 0, 0.39, 0] });
-      });
-    },
-  });
-
+  nuxtApp.vueApp.use(VueScrollTo)
   return {
     provide: {
       scrollTo: VueScrollTo.scrollTo
